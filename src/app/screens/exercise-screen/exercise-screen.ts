a8b9087c77af9b6f065dd381d134df4ce7621ecd
zach brown <bbrown64506@gmail.com>
2025-10-06 19:19:12 -0500

    exercise work

--- src/app/screens/exercise-screen/exercise-screen.ts
+++ src/app/screens/exercise-screen/exercise-screen.ts
@@ -1,14 +1,8 @@
-import {Component, computed, inject, signal} from '@angular/core';
+import {Component, computed, inject, resource, signal} from '@angular/core';
 import {IonicModule} from "@ionic/angular";
 import {ExerciseService} from '../../services/exercise.service';
-import {toObservable, toSignal} from '@angular/core/rxjs-interop';
-import {catchError, debounceTime, distinctUntilChanged, map, of, startWith, switchMap} from 'rxjs';
+import {lastValueFrom} from 'rxjs';
 import {FormsModule} from '@angular/forms';
-
-type Loading = { state: 'loading' };
-type ErrorS = { state: 'error'; error: unknown };
-type Ready = { state: 'ready'; data: Exercise[] };
-export type SearchState = Loading | ErrorS | Ready;
 
 @Component({
   selector: 'app-exercise-screen',
@@ -32,32 +26,16 @@ export class ExerciseScreen {
     limit: this.pageSize(),
   }));
 
-  readonly result = toSignal(
-    toObservable(this.searchParams).pipe(
-      debounceTime(250),
-      distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b)),
-      switchMap(p => {
-        return this.exerciseService.searchExercises(p).pipe(
-            map<Exercise[], SearchState>(data => ({ state: 'ready', data })),
-            catchError(err => of<SearchState>({ state: 'error', error: err })),
-          )
-        }
-      ),
-      startWith<SearchState>({ state: 'loading' }),
-    ),
-    { initialValue: { state: 'loading' } },
-  );
-
-
-  isLoading(s: SearchState | undefined): s is Loading {
-    return s?.state === 'loading';
-  }
-
-  isError(s: SearchState | undefined): s is ErrorS {
-    return s?.state === 'error';
-  }
-
-  isReady(s: SearchState | undefined): s is Ready {
-    return s?.state === 'ready';
-  }
+  readonly exercises = resource({
+    params: () => ({
+      query: this.query(),
+      page: this.page(),
+      limit: this.pageSize(),
+    }),
+    loader: ({params}) => lastValueFrom(this.exerciseService.searchExercises({
+      query: params.query,
+      page: params.page,
+      limit: params.limit,
+    })),
+  })
 }
