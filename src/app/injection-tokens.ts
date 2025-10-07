d5eadf15e25b36c69e915770113147db8ca680bd
zach brown <bbrown64506@gmail.com>
2025-10-07 12:36:59 -0500

    fix

--- src/app/injection-tokens.ts
+++ src/app/injection-tokens.ts
@@ -1,4 +1,5 @@
 import {InjectionToken} from '@angular/core';
 import {SupabaseClient} from '@supabase/supabase-js';
+import {Database} from './util/database';
 
-export const SUPABASE = new InjectionToken<SupabaseClient>('supabase-client');
+export const SUPABASE = new InjectionToken<SupabaseClient<Database>>('supabase-client');
