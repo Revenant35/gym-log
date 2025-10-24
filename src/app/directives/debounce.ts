cbd8ad51d568c2fc1fccc264fb4ee455ea271f31
zach brown <bbrown64506@gmail.com>
2025-10-24 11:08:16 -0500

    debounce directive

---
+++ src/app/directives/debounce.ts
@@ -0,0 +1,32 @@
+import { Directive, input, model, numberAttribute } from '@angular/core';
+
+@Directive({
+  selector: 'input [debounceTime]',
+  host: {
+    '[value]': 'value()',
+    '(input)': 'handleInput($event)',
+  },
+})
+export class Debounce {
+  #debounceTimer?: ReturnType<typeof setTimeout>;
+
+  readonly debounceTime = input(0, { transform: numberAttribute });
+  readonly value = model<string>();
+
+  handleInput(event: Event): void {
+    if (!(event.target instanceof HTMLInputElement)) {
+      // TODO: Log a warning
+      return;
+    }
+
+    const value = event.target.value;
+
+    clearTimeout(this.#debounceTimer);
+
+    if (!value || !this.debounceTime()) {
+      this.value.set(value);
+    } else {
+      this.#debounceTimer = setTimeout(() => this.value.set(value), this.debounceTime());
+    }
+  }
+}
